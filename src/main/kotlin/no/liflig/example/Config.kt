1707be5575362948bfcee56d617c64bb241c13af
Kristian Rekstad <krirek@msn.com>
2022-02-25 10:09:07 +0100

    Pass git commit to maven. Enforce no snapshot dependencies (#4)
    * Pass git commit to maven. Enforce no snapshot dependencies
    
    The commit must be sent from Jenkins to maven for it to be included
    in the resource filtering.
    
    * Add build info to properties
    
    * Add build info to Config
    
    * Log build info
    
    * Fix missing imports
    
    * Fix casing
    
    * Fix lint

--- src/main/kotlin/no/liflig/example/Config.kt
+++ src/main/kotlin/no/liflig/example/Config.kt
@@ -1,7 +1,9 @@
 package no.liflig.example
 
+import no.liflig.properties.intRequired
 import no.liflig.properties.loadProperties
 import no.liflig.properties.stringNotEmpty
+import no.liflig.properties.stringNotNull
 
 /**
  * Loads config using Liflig Properties
@@ -11,5 +13,19 @@ import no.liflig.properties.stringNotEmpty
 object Config {
   private val properties = loadProperties()
 
+  val build = Build(
+    timestamp = properties.stringNotNull("build.timestamp"),
+    commit = properties.stringNotNull("build.commit"),
+    branch = properties.stringNotNull("build.branch"),
+    buildNumber = properties.intRequired("build.number")
+  )
+
   val exampleProp = properties.stringNotEmpty("example.prop")
 }
+
+data class Build(
+  val timestamp: String,
+  val commit: String,
+  val branch: String,
+  val buildNumber: Int,
+)
